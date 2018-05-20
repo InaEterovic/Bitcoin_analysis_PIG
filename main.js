086192986757243611bf2e15cdd6bec6d56234f4
Rodrigo Torrico Del Castillo <rodrigo.torricodelcastillo@gmail.com>
2018-05-20 13:24:28 -0400

    WIP save info in files

--- main.js
+++ main.js
@@ -1,11 +1,58 @@
-console.log('hola');
 
-let Twit = require('twit');
-const config = require('./config');
+//---libraries-----------------------------------------------/
 
-let twit = new Twit(config);
+const Twit = require('twit');  //import the library twit
+const config = require('./config');  //import config file
+const fs = require('fs'); //import librari to save files int txt
 
-twit.get('search/tweets', { q: 'bitcoin lang:es', count: 2 },
- function(err, data, response) {
-    console.log(data)
-  })
+//--params-----------------------------------------------------/
+
+// params to save the file
+  const path = '/Users/rodrigotorricodelcastillo/Desktop/test';
+
+//twiter query params
+
+  twitList = [];
+
+  let params = { 
+    q: 'bitcoin lang:es ',
+    count: 5
+  };
+
+//-functions--------------------------------------------------/
+
+//twitter functions
+
+  function consoleTwits(err, data, response) {
+    console.log(data.statuses);
+  };
+
+  function setTwitList(err, data, response) {
+    for ( let i = 0; i < data.statuses.length; i++ ){
+      twitList.push(data.statuses[i].id);
+      twitList.push(data.statuses[i].text);
+    }
+    saveFile(path,twitList);
+  }
+
+
+
+//file tunctions:
+
+  function saveFile(path,content) {
+    fs.writeFile( path, content, saveStatus);
+  }
+
+  function saveStatus(err) {
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log("The file was saved!");
+    } 
+  }; 
+
+//--------------------------------------------------------/
+
+let twit = new Twit(config); // new instance
+twit.get('search/tweets', params, setTwitList);
+
